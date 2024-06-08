18243b65405266de3afb8fb48d9983bcdbce319b
Sajjadhussain197 <sajjadhussain@9582@gmail.com>
2024-06-08 18:20:28 +0500

    Add new feature

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
+// const qs = require('qs')
 import qs from "qs";
-// import qs from "qs";
 
 import { twMerge } from "tailwind-merge";
 
